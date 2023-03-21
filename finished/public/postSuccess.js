20bf3ab67187f10396e94d6ef6a8a64d5bcd1035
ramdhanyA <ariqathallah38@gmail.com>
2023-03-21 12:10:21 +0700

    added profile preview formatting on login.html after selecting the picture from GDrive

--- finished/public/postSuccess.js
+++ finished/public/postSuccess.js
@@ -72,7 +72,7 @@ function handleAuthClick() {
     }
     accessToken = response.access_token;
     // document.getElementById('signout_button').style.visibility = 'visible';
-    document.getElementById('authorize_button').innerText = 'Refresh';
+    document.getElementById('authorize_button').innerText = 'Change Selected Picture';
     await createPicker();
   };
 
